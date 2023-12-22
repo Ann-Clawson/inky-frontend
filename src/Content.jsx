a846ceb6d29800bb6c56d0ad507ccfbd9059bf9e
Ann Clawson <annmasonclawson@gmail.com>
2023-12-22 14:00:30 -0800

    creates tattooer log in component

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,36 +4,9 @@ import { UserSignup } from "./UserSignup";
 import { UserLogin } from "./UserLogin";
 import { UserLogout } from "./UserLogout";
 import { TattooerSignup } from "./TattooerSignup";
+import { TattooerLogin } from "./TattooerLogin";
 
 export function Content() {
-  // TATTOOER LOG FUNCTIONS
-  // const jwt = localStorage.getItem("jwt");
-  // if (jwt) {
-  //   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
-  // }
-
-  // const [errors, setErrors] = useState([]);
-
-  const handleTattooerLogInSubmit = (event) => {
-    event.preventDefault();
-    setErrors([]);
-    const params = new FormData(event.target);
-    axios
-      .post("http://localhost:3000/tattooer_sessions.json", params)
-      .then((response) => {
-        console.log(response.data);
-        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
-        localStorage.setItem("jwt", response.data.jwt);
-        event.target.reset();
-        // window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
-      })
-      .catch((error) => {
-        console.log(error.response);
-        setErrors(["Invalid email or password"]);
-      });
-  };
-  // END OF TATTOOER LOG FUNCTIONS
-
   // USER UPDATE FUNCTIONS
   const handleUserUpdateSubmit = (event) => {
     event.preventDefault();
@@ -45,28 +18,6 @@ export function Content() {
 
   return (
     <div>
-      {/* TATTOOER LOG IN */}
-      <div id="login">
-        <h1>Tattooer Log In</h1>
-        <ul>
-          {errors.map((error) => (
-            <li key={error}>{error}</li>
-          ))}
-        </ul>
-        <form onSubmit={handleTattooerLogInSubmit}>
-          <div>
-            Email: <input name="email" type="email" />
-          </div>
-          <div>
-            Password: <input name="password" type="password" />
-          </div>
-          <button type="submit">Login</button>
-          <a href="#" onClick={handleClick} id="logout">
-            Log Out
-          </a>
-        </form>
-      </div>
-      {/* END TATTOOER LOG IN */}
       <br />
       <br />
       <hr />
