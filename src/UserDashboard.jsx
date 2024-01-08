355b46318d70f5e94c1981b1be5696bf92fe1b32
Ann Clawson <annmasonclawson@gmail.com>
2024-01-08 08:45:51 -0800

    adds table to user dashboard

--- src/UserDashboard.jsx
+++ src/UserDashboard.jsx
@@ -21,6 +21,21 @@ export function UserDashboard() {
     <div>
       <h1>Howdy {currentUser.first_name}!</h1>
       <h4>Here is a list of your applications:</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Application Date</th>
+            <th>Amount</th>
+            <th>Tattooer</th>
+            <th>Date of Apppointment</th>
+            <th>Tattoo Description</th>
+            <th>App Status</th>
+            <th>Loan Term</th>
+            <th>Interest Rate</th>
+            <th>Monthly Payment</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
