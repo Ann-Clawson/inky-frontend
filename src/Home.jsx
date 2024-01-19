3b1e1edca4c7e2d6a91ede4c2e250bb35b7f4baf
Ann Clawson <annmasonclawson@gmail.com>
2024-01-18 16:14:53 -0800

    centers banner vertically and horizontally on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,19 @@
 export function Home() {
   return (
     <div className="home" id="home">
-      <div className="banner">
-        <h1>
-          ink(y) is for everyone<span> &copy;</span>
-        </h1>
-      </div>
-      <div className="">
-        <p>
-          Ink should be for everyone. <br />
-          And, with fair and equitable lending practices and fast and secure payments, <br />
-          <strong>ink(y)</strong> is for everyone, too.
-        </p>
+      <div className="banner-container">
+        <div className="banner">
+          <h1>
+            ink(y) is for everyone<span> &copy;</span>
+          </h1>
+        </div>
+        <div className="">
+          <p>
+            Ink should be for everyone. <br />
+            And, with fair and equitable lending practices and fast and secure payments, <br />
+            <strong>ink(y)</strong> is for everyone, too.
+          </p>
+        </div>
       </div>
     </div>
   );
