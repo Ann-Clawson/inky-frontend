692ae3d4e159e068c80ce1062037fca8e6f8888d
Ann Clawson <annmasonclawson@gmail.com>
2024-01-29 20:10:39 -0800

    adds button to banner

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,6 +14,7 @@ export function Home() {
             <strong>ink(y)</strong> is for everyone, too.
           </p>
         </div>
+        <button>Apply for Tattoo Financing</button>
       </div>
     </div>
   );
