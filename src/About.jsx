66d21da7b8d311ae40c870a42d8e2d24ed36eabd
Ann Clawson <annmasonclawson@gmail.com>
2024-02-20 12:56:52 -0800

    formats lorem on about page

--- src/About.jsx
+++ src/About.jsx
@@ -8,14 +8,17 @@ export function About() {
       <p className="about-support">
         Synth ennui semiotics mustache pickled, biodiesel food truck cosby sweater readymade mixtape letterpress
         pour-over leggings. Food truck freegan vinyl thundercats, post-ironic ennui wes anderson banh mi four loko synth
-        photo booth authentic 3 wolf moon. Sriracha pinterest sustainable jean shorts vinyl, beard echo park squid
-        mustache cliche artisan cray pickled whatever.
+        photo booth authentic 3 wolf moon.
       </p>
       <p className="about-support">
         Banksy dreamcatcher bushwick keytar, truffaut tofu scenester iphone bespoke marfa four loko forage american
         apparel blog. Umami lo-fi, mlkshk small batch williamsburg cred. Godard shoreditch direct trade ethnic art
         party. Ethnic williamsburg echo park direct trade, messenger bag whatever 3 wolf moon cosby sweater ennui
-        organic. Quinoa you probably haven&apos;t heard of them ennui, trust fund tofu banh mi flexitarian bicycle
+        organic.
+      </p>
+      <p className="about-support">
+        Sriracha pinterest sustainable jean shorts vinyl, beard echo park squid mustache cliche artisan cray pickled
+        whatever.Quinoa you probably haven&apos;t heard of them ennui, trust fund tofu banh mi flexitarian bicycle
         rights echo park gentrify messenger bag.
       </p>
     </div>
