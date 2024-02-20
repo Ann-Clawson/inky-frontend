c9226a52d5343cff9378063fe33c74137d9b953d
Ann Clawson <annmasonclawson@gmail.com>
2024-02-20 12:11:21 -0800

    adds lorem to about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,23 @@
 export function About() {
   return (
-    <div>
-      <h3 className="about">
-        At <strong className="about-inky">inky</strong> we strive to make art accessible to everyone,
+    <div className="about">
+      <h2>
+        At <strong className="about-inky">inky</strong> we make it our business to increase accessibility to art
         <br /> and support small business growth.
-      </h3>
+      </h2>
+      <p className="about-support">
+        Synth ennui semiotics mustache pickled, biodiesel food truck cosby sweater readymade mixtape letterpress
+        pour-over leggings. Food truck freegan vinyl thundercats, post-ironic ennui wes anderson banh mi four loko synth
+        photo booth authentic 3 wolf moon. Sriracha pinterest sustainable jean shorts vinyl, beard echo park squid
+        mustache cliche artisan cray pickled whatever.
+      </p>
+      <p className="about-support">
+        Banksy dreamcatcher bushwick keytar, truffaut tofu scenester iphone bespoke marfa four loko forage american
+        apparel blog. Umami lo-fi, mlkshk small batch williamsburg cred. Godard shoreditch direct trade ethnic art
+        party. Ethnic williamsburg echo park direct trade, messenger bag whatever 3 wolf moon cosby sweater ennui
+        organic. Quinoa you probably haven&apos;t heard of them ennui, trust fund tofu banh mi flexitarian bicycle
+        rights echo park gentrify messenger bag.
+      </p>
     </div>
   );
 }
