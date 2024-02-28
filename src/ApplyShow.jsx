8ab707227ca88e0674fc118e673c19631bd8eb5e
Ann Clawson <annmasonclawson@gmail.com>
2024-02-28 15:01:37 -0800

    styles apply modal and adds link to sign up

--- src/ApplyShow.jsx
+++ src/ApplyShow.jsx
@@ -1,8 +1,10 @@
 export function ApplyShow() {
   return (
-    <div>
-      <h1>You must be logged in to do that, bruh!</h1>
-      <h2>Sign up and log in.</h2>
+    <div className="applyModal">
+      <h2>You must be logged in to do that, bruh!</h2>
+      <h3>
+        <a href="/usersignup">Sign up</a> and log in.
+      </h3>
     </div>
   );
 }
