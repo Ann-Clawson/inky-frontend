de713140eae4991a359c9e8365b2ced35d2d7649
Ann Clawson <annmasonclawson@gmail.com>
2024-03-28 19:34:37 -0700

    works on keeping signup form out of footer

--- src/UserSignup.jsx
+++ src/UserSignup.jsx
@@ -14,31 +14,33 @@ export function UserSignup() {
   return (
     <div className="user-signup">
       <div className="signup-form-container">
-        <h1>Users Sign Up</h1>
-        <form onSubmit={handleUserSubmit}>
-          <div>
-            First Name <input name="first_name" type="text" required />
-          </div>
-          <div>
-            Last Name <input name="last_name" type="text" required />
-          </div>
-          <div>
-            Address <input name="address" type="text" required />
-          </div>
-          <div>
-            Phone Number <input name="phone_number" type="text" required />
-          </div>
-          <div>
-            Email <input name="email" type="email" required />
-          </div>
-          <div>
-            Password <input name="password" type="password" required />
-          </div>
-          <div>
-            Password confirmation <input name="password_confirmation" type="password" required />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <div>
+          <h1>Users Sign Up</h1>
+          <form onSubmit={handleUserSubmit}>
+            <div>
+              First Name <input name="first_name" type="text" required />
+            </div>
+            <div>
+              Last Name <input name="last_name" type="text" required />
+            </div>
+            <div>
+              Address <input name="address" type="text" required />
+            </div>
+            <div>
+              Phone Number <input name="phone_number" type="text" required />
+            </div>
+            <div>
+              Email <input name="email" type="email" required />
+            </div>
+            <div>
+              Password <input name="password" type="password" required />
+            </div>
+            <div>
+              Password confirmation <input name="password_confirmation" type="password" required />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
