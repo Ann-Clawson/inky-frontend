4460bb9a4db583346f4662801b84b3a95f292842
Ann Clawson <annmasonclawson@gmail.com>
2024-06-20 14:13:24 -0700

    imports activityshow

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
-import { Modal } from "./Modal";
-import { SignupShow } from "./SignupShow";
+import { ActivityShow } from "./ActivityShow";
 import { ApplyShow } from "./ApplyShow";
 import { LoginShow } from "./LoginShow";
+import { SignupShow } from "./SignupShow";
 import { BrowserRouter } from "react-router-dom";
+import { Modal } from "./Modal";
 import { useState } from "react";
 import axios from "axios";
 
